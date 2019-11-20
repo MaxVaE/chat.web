cc20903bad59e903a09c4b0bca1e38e2290704cb
Maksim Valyakin <max908665@gmail.com>
2019-11-20 19:10:00 +0500

    Add ChatComponent

--- src/components/lib/ChatComponent/ChatComponent.js
+++ src/components/lib/ChatComponent/ChatComponent.js
@@ -15,11 +15,24 @@ export default {
     ...mapActions({
       sendMessage: 'messages/send'
     }),
+    noEnter (e) {
+      if (e.keyCode === 13 && !e.shiftKey) {
+        e.preventDefault()
+        this.handleSendClick()
+      }
+    },
     handleSendClick () {
       if (this.text) {
-        this.sendMessage(this.text)
-        this.text = ''
+        if (/\S/.test(this.text)) {
+          this.sendMessage(this.text)
+          this.text = ''
+        }
       }
     }
+
+  },
+  updated () {
+    let length = this.$refs.element.length
+    this.$refs.element[length - 1].scrollIntoView(false)
   }
 }
